fa54fe1f6ff797e830db97a4fa5c2193a29d6612
heshan <heshanu97@gmail.com>
2020-07-15 10:15:42 +0530

    save arezzo files

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {HeaderComponent} from './shared/header/header.component';
+import {AboutusComponent} from './shared/aboutus/aboutus.component';
+import {FooterComponent} from './shared/footer/footer.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',redirectTo:'/aboutus',pathMatch:'full'},
+  {path:'aboutus',component:AboutusComponent},
+  {path:'header',component:HeaderComponent},
+  {path:'footer',component:FooterComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,25 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './footer.component';
+import { AboutusComponent } from './shared/aboutus/aboutus.component';
+import { CustoommaterialModule } from './custoommaterial.module';
+import { MatCardModule } from '@angular/material/card';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    AboutusComponent,
+    MatCardModule
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CustoommaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
