cf9bd93678b77356f49fff9b20d1dbcc008120ff
heshan <heshanu97@gmail.com>
2020-07-18 08:52:44 +0530

    test

---
+++ src/app/shared/aboutus/aboutus.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+
+@Component({
+  selector: 'app-aboutus',
+  templateUrl: './aboutus.component.html',
+  styleUrls: ['./aboutus.component.css']
+})
+export class AboutusComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
